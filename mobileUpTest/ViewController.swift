03693b890b5a927e64be7ef98f38fca91a4c53c1
Vitalii-Vishnyakov <vitalii_vish@mail.ru>
2022-03-25 13:48:26 +0300

    first commit

---
+++ mobileUpTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobileUpTest
+//
+//  Created by Виталий on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
